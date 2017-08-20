e99ca5dee80e0447aba5009a2463c911607297f0
devBae <z1004man@naver.com>
2017-08-20 13:44:48 +0900

    testCase 작성 및 detach 관련 에러가 발생해서 도메인 cascade 옵션 삭제.(아마 외래키가 안걸려있어서 그런가로 판단 -_-;;)

--- src/test/java/org/phpbae/IndexRestMockTest.java
+++ src/test/java/org/phpbae/IndexRestMockTest.java
@@ -9,23 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by Administrator on 2017-08-12.
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(secure = false)
 public class IndexRestMockTest {
 
     @MockBean
@@ -42,17 +45,23 @@ public class IndexRestMockTest {
     public void insertDepartmentTest() throws Exception {
         when(this.departmentRestService.insertDepartment("배님",1)).thenReturn(department);
         this.mockMvc.perform(put("/api/departments/insert_department/배님/1").accept(MediaType.APPLICATION_JSON).characterEncoding("utf8")
-        ).andDo(print())
+        )
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
     public void getDepartmentTest() throws Exception {
-        //given(this.departmentRestService.getDepartment(1)).willReturn(department); // getDepartment 메소드에 인자값 1이 입력될 경우 .willReturn(getDepartment); // getDepartment 객체를 리턴한다.
-        when(this.departmentRestService.getDepartment(1)).thenReturn(department);
-        this.mockMvc.perform(get("/api/departments/1").accept(MediaType.APPLICATION_JSON).characterEncoding("utf8")
-        ).andDo(print())
-                .andExpect(status().isOk());
+        //given(this.departmentRestService.getDepartment(1)).willCallRealMethod(); // getDepartment 메소드에 인자값 1이 입력될 경우 .willReturn(getDepartment); // getDepartment 객체를 리턴한다.
+        //when(this.departmentRestService.getDepartment(1)).thenReturn(department);
+        this.mockMvc.perform(get("/api/departments/{departmentIdx}", 1).characterEncoding("utf8")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(redirectedUrl(null))
+                .andExpect(jsonPath("$.department_name").isNotEmpty())
+                .andExpect(jsonPath("$.department_name").value("인사팀"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
     }
 
 }
